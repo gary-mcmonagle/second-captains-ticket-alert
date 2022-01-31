37014fa0d5fe5acbf4e930806c17f1a3aeae6d18
Gary McMonagle <garybmcmonagle@gmail.com>
2022-01-31 20:15:17 +0000

    edit email

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
+const write = require('write')
 
 const cheerio = require('cheerio')
 const run = async () => {
@@ -12,13 +13,7 @@ const run = async () => {
     $liveEvents.each(($l, $el) => {
         venues.push(cheerio.load($el).text());
     })
-    await fs.writeFile("/email.html", venues.join('/n'), function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    }); 
-
+    write.sync('email.htm', venues.join('\n'), { newline: true }); 
 }
 
 run().then(() => console.log('done'))
